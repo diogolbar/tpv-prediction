25f63dda45dc45f7904fdafe4975f58957c7da52
tesla <diogolbar@gmail.com>
2024-05-02 10:52:06 -0300

    refactor: settings.py

--- src/tpv_prediction/settings.py
+++ src/tpv_prediction/settings.py
@@ -1,9 +1,12 @@
-"""Project settings. There is no need to edit this file unless you want to change values
-from the Kedro defaults. For further information, including these default values, see
+"""
+Project settings. There is no need to edit this file unless you want to
+change values from the Kedro defaults. For further information,
+including these default values, see
 https://docs.kedro.org/en/stable/kedro_project_setup/settings.html."""
 
 # Instantiated project hooks.
-# For example, after creating a hooks.py and defining a ProjectHooks class there, do
+# For example, after creating a hooks.py and defining a ProjectHooks
+# class there, do
 # from pandas_viz.hooks import ProjectHooks
 
 # Hooks are executed in a Last-In-First-Out (LIFO) order.
@@ -30,12 +33,12 @@ from kedro.config import OmegaConfigLoader  # noqa: E402
 CONFIG_LOADER_CLASS = OmegaConfigLoader
 # Keyword arguments to pass to the `CONFIG_LOADER_CLASS` constructor.
 CONFIG_LOADER_ARGS = {
-      "base_env": "base",
-      "default_run_env": "local",
-#       "config_patterns": {
-#           "spark" : ["spark*/"],
-#           "parameters": ["parameters*", "parameters*/**", "**/parameters*"],
-#       }
+    "base_env": "base",
+    "default_run_env": "local",
+    #       "config_patterns": {
+    #           "spark" : ["spark*/"],
+    # "parameters": ["parameters*", "parameters*/**", "**/parameters*"],
+    #       }
 }
 
 # Class that manages Kedro's library components.
